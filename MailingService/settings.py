ddf260a03c7c4d6570faa038f95a11c8c69dda07
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-08-20 14:21:12 +0300

    Update settings.py

--- MailingService/settings.py
+++ MailingService/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'mail.apps.MailConfig',
 ]
 
@@ -66,6 +67,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 WSGI_APPLICATION = 'MailingService.wsgi.application'
 

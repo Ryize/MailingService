9ae682541e75bbf0a451ee6f4429cb4f3001361a
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-08-28 20:18:55 +0300

    Added display of the Mail model in the admin panel

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from mail.models import Mail
+
+@admin.register(Mail)
+class MailAdmin(admin.ModelAdmin):
+    fields = ('id', 'email', 'subject', 'message', 'from_email', 'is_mass', 'created_at', )
+    list_display = ('id', 'subject', 'from_email', 'is_mass', 'created_at', )
+    list_display_links = ('id', 'subject', 'is_mass', 'created_at',)
+    list_filter = ('is_mass', 'created_at',)
+    readonly_fields = ('created_at',)
+    empty_value_display = '-пустой-'
+    list_per_page = 64
+    list_max_show_all = 8
+    search_fields = ['subject', 'is_mass', 'email', 'message']
--- mail/models.py
+++ mail/models.py
@@ -6,5 +6,8 @@ class Mail(models.Model):
     subject = models.CharField(max_length=256)
     message = models.CharField(max_length=2048)
     from_email = models.EmailField(max_length=64)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     is_mass = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.subject}. Массовая: {self.is_mass}'

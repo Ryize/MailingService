abe5da674ce252c2d34508f4fb1016a934ae5097
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-08-29 12:31:08 +0300

    Added verbose_name for fields in the Mail model. Minor bug fixing

--- mail/models.py
+++ mail/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Mail(models.Model):
-    email = models.TextField()
-    subject = models.CharField(max_length=256)
-    message = models.CharField(max_length=2048)
-    from_email = models.EmailField(max_length=64)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_mass = models.BooleanField(default=False)
+    email = models.TextField(verbose_name="Почты(ы)")
+    subject = models.CharField(max_length=256, verbose_name="Тема")
+    message = models.CharField(max_length=2048, verbose_name="Сообщение")
+    from_email = models.EmailField(max_length=64, verbose_name="Отправитель")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    is_mass = models.BooleanField(default=False, verbose_name="Массовая")
 
     def __str__(self):
         return f'{self.subject}. Массовая: {self.is_mass}'
